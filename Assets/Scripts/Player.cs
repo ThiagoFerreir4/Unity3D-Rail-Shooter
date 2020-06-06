6c0512d220908888bd5344067fbcfd7c3bc44840
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-06-06 12:21:55 +0300

    08. Input sensitivity & gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +14,9 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        print(xOffsetThisFrame);
+
 	}
 }
