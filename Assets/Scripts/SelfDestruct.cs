dbd67716cea2cd767fad4fc6f685125c246a79b7
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-06-09 01:16:10 +0300

    21b. Instantiating at Runtime saved script

--- Assets/Scripts/SelfDestruct.cs
+++ Assets/Scripts/SelfDestruct.cs
@@ -6,7 +6,7 @@ public class SelfDestruct : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Destroy(gameObject, 5f);
+        Destroy(gameObject, 5f); // todo allow customization
 	}
 	
 	// Update is called once per frame
